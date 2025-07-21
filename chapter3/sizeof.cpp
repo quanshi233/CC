01e43710df69ffa3e9c1c340b86e8d9a514abb64
石泉 <moxiu233@gmail.com>
2025-07-21 18:50:41 +0800

    update

---
+++ chapter3/sizeof.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <climits>
+int main(){
+    using namespace std;
+    int n_int = INT_MAX;
+    int n_min_int = INT_MIN;
+    short n_short = SHRT_MAX;
+    long n_long = LONG_MAX;
+    long long n_llong = LLONG_MAX;
+    cout << "int is " << sizeof n_int << " bytes." <<endl;
+    cout << "short is " << sizeof n_short  << " bytes." <<endl;
+    cout << "long is " << sizeof n_long << " bytes." <<endl;
+    cout << "llong is " << sizeof n_llong  << " bytes." <<endl;
+    cout << endl;
+    cout << "Maximum values:" <<endl;
+    cout << "int: " << n_int << endl;
+    cout << "short: " << n_short << endl;
+    cout << "long: " << n_long << endl;
+    cout << "llong: " << n_long << endl;
+    cout << "Minimum int value = " << n_min_int << endl;
+    cout << "Bits per byte = " <<CHAR_BIT << endl;
+    return 0;
+}
