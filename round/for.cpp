a1e4bef28e9c9f7cd522ce107c34bc66fada94fe
moxiu <moxiu233@gmail.com>
2025-07-23 09:44:35 +0800

    update

--- round/for.cpp
+++ round/for.cpp
@@ -1,20 +1,35 @@
 #include <iostream>
+double smaller(double, double);
+double bigger(double, double);
 int main(){
     int a, b;
     long long c=0;
     using namespace std;
     cout << "Please enter two number.\n";
     cin >> a >> b;
-    int number1 = a, number2 = b;
     if(a>=b){
-        for(a; a>=b; a--){
-            c += a;
+        for(int val = a; val >= b; val--){
+            c += val;
         }
     }else{
-        for(a; a<=b; a++){
-            c += a;
+        for(int val; val <= b; val++){
+            c += val;
         }
     }
-    cout << "Sum of " << number1 <<" to " << number2 << " is " << c <<endl;
+    cout << "Sum of " << smaller(a, b) <<" to " << bigger(a, b) << " is " << c <<endl;
     return 0;
+}
+double smaller(double a, double b){
+    if(a>=b){
+        return b;
+    }else{
+        return a;
+    }
+}
+double bigger(double a, double b){
+    if(a>=b){
+        return a;
+    }else{
+        return b;
+    }
 }
