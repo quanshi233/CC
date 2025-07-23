8a13e57a197b71a2cb4d0f5eaff12664008565ae
moxiu <moxiu233@gmail.com>
2025-07-23 16:37:44 +0800

    update

---
+++ scopetest.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int test;
+    int sum = 0;
+    for(int test=0; test <= 10; test++){
+        sum += test;
+    }
+    cout <<"test: "<<test <<endl <<"sum: "<<sum <<endl;
+    return 0;
+}
