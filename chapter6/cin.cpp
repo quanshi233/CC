c77a712ac75811795489a1653b2a923796d2ba00
moxiu <moxiu233@gmail.com>
2025-07-30 18:14:00 +0800

    update

---
+++ chapter6/cin.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+int main(){
+    using namespace std;
+    int a;
+    double b;
+    char c;
+    string d;
+    cin >>a >>b >>c >>d;
+    cout<<a<<endl
+        <<b<<endl
+        <<c<<endl
+        <<d<<endl;
+    return 0;
+}
---
+++ chapter6/test.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main()
+{
+    std::cout << "Good\vmorning.\nThis is moxiu";
+}
